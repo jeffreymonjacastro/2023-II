b69594fcf67f27ba83ec502ff2fd0299e8bd882c
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-08-16 14:31:44 -0500

    Create template.cpp

---
+++ CPC/Codeforces/template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define cpu() ios::sync_with_stdio(false); cin.tie(nullptr);
+typedef long long ll;
+typedef long double lld;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define F0R(i, a, b) for (int i=a; i<b; i++)
+#define FOR(i, a) for (int i=0; i<a; i++)
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+const int MAX = 1e9 + 7;
+const int MIN = -1e9 - 7;
+
+void solve(){
+  cout << "Hello World" << endl;
+}
+
+int main(){
+  int t; 
+  cin>>t;
+
+  while(t--)
+    solve();
+
+  return 0;
+}
