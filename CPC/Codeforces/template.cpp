e906294f3940aa5ee65ba045750eecf3f68602ad
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-08-23 15:30:35 -0500

    new template

--- CPC/Codeforces/template.cpp
+++ CPC/Codeforces/template.cpp
@@ -2,34 +2,25 @@
 
 using namespace std;
 
-#define cpu() ios::sync_with_stdio(false); cin.tie(nullptr);
+#define cpu() ios::sync_with_stdio(false); cin.tie(nullptr)
 typedef long long ll;
 typedef long double lld;
-typedef vector<int> vi;
-typedef pair<int, int> pi;
 
-#define F0R(i, a, b) for (int i=a; i<b; i++)
-#define FOR(i, a) for (int i=0; i<a; i++)
-#define mp make_pair
-#define pb push_back
-#define f first
-#define s second
-#define lb lower_bound
-#define ub upper_bound
+#define rep(i, a, b) for (ll i=a; i<b; i++);
+#define rev(i, a, b) for (ll i=a; i>=b; i--);
 
-const int MAX = 1e9 + 7;
-const int MIN = -1e9 - 7;
+const int MAX = INT32_MAX;
+const int MIN = INT32_MIN;
 
 void solve(){
-  cout << "Hello World" << endl;
+  
 }
 
 int main(){
-  int t; 
+  cpu();
+  int t=1; 
   cin>>t;
-
+  
   while(t--)
     solve();
-
-  return 0;
 }
