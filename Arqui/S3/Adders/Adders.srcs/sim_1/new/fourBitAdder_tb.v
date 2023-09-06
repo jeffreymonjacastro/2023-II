ad16e10040824c8c7dbafa64ec409019c570a920
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-09-06 14:17:44 -0500

    Done 4 bit Adder

--- Arqui/S3/Adders/Adders.srcs/sim_1/new/fourBitAdder_tb.v
+++ Arqui/S3/Adders/Adders.srcs/sim_1/new/fourBitAdder_tb.v
@@ -27,12 +27,18 @@ module fourBitAdder_tb;
   fourBitAdder fbA(s0, s1, s2, s3, co, a0, a1, a2, a3, b0, b1, b2, b3, cin);
 
   initial begin
-    a0 = 0; a1 = 1; a2 = 1; a3 = 0;
-    b0 = 0; b1 = 1; b2 = 0; b3 = 1;
+    a0 = 0; a1 = 1; a2 = 0; a3 = 0;
+    b0 = 1; b1 = 1; b2 = 0; b3 = 0;
     cin = 0;
     #10
-    a0 = 0; a1 = 0; a2 = 0; a3 = 1;
-    b0 = 0; b1 = 1; b2 = 1; b3 = 1;
+    a0 = 1; a1 = 0; a2 = 0; a3 = 0;
+    b0 = 1; b1 = 1; b2 = 1; b3 = 0;
+    #10
+    a0 = 1; a1 = 0; a2 = 1; a3 = 0;
+    b0 = 0; b1 = 1; b2 = 0; b3 = 1;
+    #10
+    a0 = 1; a1 = 1; a2 = 1; a3 = 1;
+    b0 = 1; b1 = 0; b2 = 0; b3 = 0;
     #10 $finish;
   end
 endmodule
--- Arqui/S3/Adders/Adders.srcs/sources_1/new/fourBitAdder.v
+++ Arqui/S3/Adders/Adders.srcs/sources_1/new/fourBitAdder.v
@@ -27,7 +27,7 @@ module fourBitAdder(s0, s1, s2, s3, co, a0, a1, a2, a3, b0, b1, b2, b3, cin);
 	wire c1, c2, c3;
 
 	fullAdder fA1(s0, c1, a0, b0, cin);
-	fullAdder fA2(s1, c2, a1, b1, c0);
-	fullAdder fA3(s2, c3, a2, b2, c1);
-	fullAdder fA4(s3, co, a3, b3, c2);
+	fullAdder fA2(s1, c2, a1, b1, c1);
+	fullAdder fA3(s2, c3, a2, b2, c2);
+	fullAdder fA4(s3, co, a3, b3, c3);
 endmodule	
